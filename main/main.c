d10483107529781840f236c9801bb8c90df3b8b1
xRetry <marcoschwarz.at@gmail.com>
2023-11-24 21:53:34 +0100

    Add wifi

--- main/main.c
+++ main/main.c
@@ -1,46 +1,53 @@
 #include <stdio.h>
-#include <inttypes.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
 #include "esp_system.h"
+#include "esp_wifi.h"
+#include "mongoose.h"
+#include "nvs_flash.h"
+#include "wifi.h"
 
-void app_main(void)
-{
+static void fn(struct mg_connection *c, int ev, void *ev_data, void *fn_data) {
+    if (ev == MG_EV_HTTP_MSG) {
+        struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+        if (mg_http_match_uri(hm, "/api/hello")) {              // On /api/hello requests,
+            mg_http_reply(c, 200, "", "{%m:%d}\n", MG_ESC("status"), 1);                   // Send dynamic JSON response
+        } else {                                                // For all other URIs,
+            mg_http_reply(c, 404, "", "{%m:%d}\n", MG_ESC("status"), 1);                   // Send dynamic JSON response
+            //struct mg_http_serve_opts opts = {.root_dir = "."};   // Serve files
+            //mg_http_serve_dir(c, hm, &opts);                      // From root_dir
+        }
+    }
+}
+
+void app_main(void) {
     printf("Hello world!\n");
 
-    /* Print chip information */
-    esp_chip_info_t chip_info;
-    uint32_t flash_size;
-    esp_chip_info(&chip_info);
-    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
-           CONFIG_IDF_TARGET,
-           chip_info.cores,
-           (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
-           (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
-           (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
-           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
-
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
-    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
-        printf("Get flash size failed");
-        return;
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
     }
+    ESP_ERROR_CHECK(ret);
 
-    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
-           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    wifi_init_sta();
+    return;
 
-    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+
+    esp_err_t err = esp_wifi_init(&config);
+    printf("%d", err);
+
+
+    return;
+    struct mg_mgr mgr;                                
+    mg_mgr_init(&mgr);                                      // Init manager
+    mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
+    for (;;) mg_mgr_poll(&mgr, 1000);                       // Infinite event loop
 }
