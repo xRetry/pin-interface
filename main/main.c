f9f48afdf19c6f027903901bc6fced697fb59077
xRetry <marcoschwarz.at@gmail.com>
2023-11-24 21:56:33 +0100

    Enable webserver

--- main/main.c
+++ main/main.c
@@ -33,19 +33,7 @@ void app_main(void) {
     ESP_ERROR_CHECK(ret);
 
     wifi_init_sta();
-    return;
 
-    ESP_ERROR_CHECK(esp_netif_init());
-    ESP_ERROR_CHECK(esp_event_loop_create_default());
-    esp_netif_create_default_wifi_sta();
-
-    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
-
-    esp_err_t err = esp_wifi_init(&config);
-    printf("%d", err);
-
-
-    return;
     struct mg_mgr mgr;                                
     mg_mgr_init(&mgr);                                      // Init manager
     mg_http_listen(&mgr, "http://0.0.0.0:8000", fn, NULL);  // Setup listener
